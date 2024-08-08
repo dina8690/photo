799981ac9cbb589e076b655659fa72f69bcd90cc
Dinara Bakirova <dinarabakirova@icloud.com>
2024-08-09 00:14:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var myAge = 34;
+
+var age = 30;
+if (myAge >= age)
+{
+    console.log("molokosos");
+} else if(age - myAge <=10) {
+    console.log("podumaem");
+}else {
+    console.log("starik");
+}
